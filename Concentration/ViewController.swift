ce4b9ed0acc17b789dc8bffe18338af6adac123d
black <ryo.0301.1999@gmail.com>
2018-12-15 14:28:54 +0900

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by 五十嵐諒 on 2018/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
